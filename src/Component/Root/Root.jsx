f223d33048b5a87ca91a5f1a24056c8cd5f92151
fatematujjohra <fatematujjohralima2@gmail.com>
2026-05-21 17:03:30 +0600

    add styling for root-main

--- src/Component/Root/Root.jsx
+++ src/Component/Root/Root.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router';
 import SideBar from '../Sidebar/Sidebar';
+import './Root.css'
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
-            <SideBar></SideBar>
+            <div className="root-main">
+                <SideBar></SideBar>
 
-
-            <Outlet></Outlet>
+                <Outlet></Outlet>
+            </div>
             
         </div>
     );
